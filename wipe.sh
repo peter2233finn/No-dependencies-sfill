92757ddfa83b3654f540fb671e55a64b53cde5c6
Peter Finn <c16407722@mydit.ie>
2020-01-29 05:31:12 +0000

    Update wipe.sh

--- wipe.sh
+++ wipe.sh
@@ -6,33 +6,64 @@ wipeFile="fileee"
 wipeStr="ABCDEFG"
 # If your having high CPU usage or crashes, set this to a small value like 0.001
 rest=0
+# blockSize sets the size of the blocks
+blockSize=1000
+# wipeFolder is the name of the folder the wipe files will be copied to
+wipeFolder="folder1"
 
 printf "Starting wipe\n"
 ctr=0
 offSet=0
+
+
 while (( ctr != wipeRuns ))
 do
-	((ctr++))
-
-	# do a charecter offset so it doesnt wipe over itself identically
+	# creates the temporary file
 	offCtr=0
-	while (( offSet > offCtr ))
-	do
-		printf "A" >> $wipeFile
-		((offCtr++))
+	for i in {0..$wipes}
+		do
+
+		# do a charecter offset so it doesnt wipe over itself identically
+		while (( offSet > offCtr ))
+		do
+			printf "A" >> $wipeFile
+			((offCtr++))
+		done
+
+		# create tempoary block
+		for i in $(seq 1 $blockSize)
+		do
+			printf $wipeStr >> tmpFile
+		done
 	done
+
+	((ctr++))
 	offCtr=0
 	((offSet++))
 	errorCtr=0
   
-  # Do the wipe
+	# create the folder
+	mkdir $wipeFolder
+	printf "Folder $wipeFolder created...\n"
+
+	fileCtr=0
 	while (( errCtr != 20 ))
 	do
-    # Should fail to add onto the file when memory is full
-		printf "$wipeStr " >> $wipeFile || (( errorCtr++))
+    		# Will copy the file until 20 errors accor (Assumption is that memory is full) when memory is full
+		cp tmpFile $wipeFolder/$wipeFile.$fileCtr || (( errorCtr++))
+		((fileCtr++))
 		sleep $rest
+		printf "file: $fileCtr created\n"
+	done
+	printf "Not enough memory to create any more files. Will fill up the remaining space/n"
+	while true
+	do
+		echo $wipeStr >> $wipeFolder/$wipeFile || break
+
 	done
 	rm $wipeFile
+	rm -r $wipeFolder
+	rm tmpFile
 	printf "completed wipe: $ctr/$wipeRuns\n"
 done
 printf "All done...\n"
